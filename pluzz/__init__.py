f9972648d4748ae8068b3a6c0955fbea7ed7105f
Guyzmo <guyzmo+github@m0g.net>
2014-05-14 03:21:07 +0200

    die badly if run with python 2.
    Signed-off-by: Guyzmo <guyzmo+github@m0g.net>

--- pluzz/__init__.py
+++ pluzz/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 import gettext
 
-t = gettext.install('pypluzz', os.path.join(os.path.dirname(__file__),'i18n'))
+if sys.version_info.major != 3:
+    raise Exception("Sorry this software only works with python 3.")
+
+gettext.install('pypluzz', os.path.join(os.path.dirname(__file__),'i18n'))
